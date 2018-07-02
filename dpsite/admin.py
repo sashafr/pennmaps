34988ddedb45edc98242881ea20d6850f6837f99
Sasha Renninger <sashafr@upenn.edu>
2018-07-02 21:05:22 +0000

    adding osm widget to admin geom form fields

--- dpsite/admin.py
+++ dpsite/admin.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.gis import forms as geoforms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 from .models import *
@@ -33,17 +34,30 @@ class MediaAdmin(ImportExportModelAdmin):
 admin.site.register(Media, MediaAdmin)
 
 class MapItemAdminForm(forms.ModelForm):
-    summary = forms.CharField(widget=CKEditorWidget())
-    description = forms.CharField(widget=CKEditorWidget())
+    summary = forms.CharField(widget=CKEditorWidget(), required=False)
+    description = forms.CharField(widget=CKEditorWidget(), required=False)
+    location1 = geoforms.MultiPointField(widget = geoforms.OSMWidget(attrs={'default_lat': 39.9526, 'default_lon': -75.1652, 'default_zoom': 12 }), required=False)
+    location2 = geoforms.MultiPolygonField(widget = geoforms.OSMWidget(attrs={'default_lat': 39.9526, 'default_lon': -75.1652, 'default_zoom': 12 }), required=False)
 
     class Meta:
         model = MapItem
         fields = '__all__'
 
-class MapItemAdmin(admin.ModelAdmin):
+class MapItemResource(resources.ModelResource):
+
+    class Meta:
+        model = MapItem
+
+class MappedMediaInline(admin.TabularInline):
+    model = MappedMedia
+    extra = 1
+
+class MapItemAdmin(ImportExportModelAdmin):
     form = MapItemAdminForm
     list_display = ('title','location_notes','status')
     list_filter = ['tags']
+    resource_class = MapItemResource
+    inlines = (MappedMediaInline, )
 
 admin.site.register(MapItem, MapItemAdmin)
 
