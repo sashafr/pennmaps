17d9ae0311bc0dbf8b69b04f20b570eb0ee2c8fc
Siyang You <siyangy@seas.upenn.edu>
2018-07-03 19:07:07 +0000

    add search function

--- dpsite/admin.py
+++ dpsite/admin.py
@@ -1,9 +1,7 @@
 from django import forms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
-from .models import *
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
+from .models import MapItem, WebSeries, Media, PartOfCity, TimePeriod, Tag, TagGroup, OverlayGroup
 
 
 # Register your models here.
@@ -11,26 +9,8 @@ from import_export.admin import ImportExportModelAdmin
 admin.site.register(Tag)
 admin.site.register(TagGroup)
 admin.site.register(OverlayGroup)
+admin.site.register(Media)
 
-class MediaAdminForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea, required=False)
-    credits = forms.CharField(widget=forms.Textarea, required=False)
-
-    class Meta:
-        model = Media
-        fields = '__all__'
-
-class MediaResource(resources.ModelResource):
-
-    class Meta:
-        model = Media
-
-class MediaAdmin(ImportExportModelAdmin):
-    form = MediaAdminForm
-    list_display = ('title', 'description')
-    resource_class = MediaResource
-
-admin.site.register(Media, MediaAdmin)
 
 class MapItemAdminForm(forms.ModelForm):
     summary = forms.CharField(widget=CKEditorWidget())
@@ -39,13 +19,13 @@ class MapItemAdminForm(forms.ModelForm):
     class Meta:
         model = MapItem
         fields = '__all__'
-
 class MapItemAdmin(admin.ModelAdmin):
     form = MapItemAdminForm
     list_display = ('title','location_notes','status')
     list_filter = ['tags']
 
 admin.site.register(MapItem, MapItemAdmin)
+
 
 admin.site.register(WebSeries)
 admin.site.register(PartOfCity)
