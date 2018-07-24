597272b83e1815a69b2fa994109d0f75320f3909
Sasha Renninger <sashafr@upenn.edu>
2018-07-24 18:31:05 +0000

    fixing url base and settings

--- digginphilly/settings-dist.py
+++ digginphilly/settings-dist.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['204.48.29.5']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
