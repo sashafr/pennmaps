a3bdb5120f9f20955544e1434dac57b4ea378cf6
Sasha Renninger <sashafr@upenn.edu>
2018-07-25 15:52:59 +0000

    cleaning up header and footer

--- dpsite/urls.py
+++ dpsite/urls.py
@@ -2,18 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.base, name = 'base'),
+	path('', views.base, name = 'home'),
 	path('dpsite/', views.mapItem),
     path('webseries/', views.webSeries, name = 'webseries'),
 	path('mediaitem/', views.mediaItem),
-	path('mediaitem/', views.mediaItem),
-    path('media/gallery/', views.mediaGallery),
+    path('media/gallery/', views.mediaGallery, name = 'mediagallery'),
     path('media/gallery/<slug:tag>/', views.mediaGallery),
     path('archive/search/', views.archiveSearch),
-    path('archive/gallery/', views.archiveGallery),
+    path('archive/gallery/', views.archiveGallery, name = 'archivegallery'),
     path('archive/gallery/<slug:tag>/', views.archiveGallery),
     path('archive/item/<int:id>/', views.archiveItem),
-    path('about/team/', views.aboutTeam),
-    path('about/project/', views.aboutProject),
-    path('map/', views.map),
+    path('about/team/', views.aboutTeam, name = 'aboutteam'),
+    path('about/project/', views.aboutProject, name = 'aboutproject'),
+    path('map/', views.map, name = 'map'),
 ]
